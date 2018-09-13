1d8a6512ec7d60a9c515b90f6c6b47dbb64406c7
Meissa Dia <meissadia@gmail.com>
2018-09-13 01:57:17 -0700

    refactor state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component }     from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
+import * as State      from './lib/AppState';
 import AppFrame        from './components/AppFrame';
 import Dashboard       from './components/Dashboard';
 import Gratitudes      from './components/Gratitudes';
@@ -22,134 +23,25 @@ import './css/App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = this.initState();
-    this.updateUserHeader = this.updateUserHeader.bind(this);
-    this.resetState = this.resetState.bind(this);
-    this.deleteFromStateArray = this.deleteFromStateArray.bind(this);
-    this.addToStateArray = this.addToStateArray.bind(this);
-    this.deleteProject = this.deleteProject.bind(this);
-    this.deletePriority = this.deletePriority.bind(this);
-    this.deleteRelationship = this.deleteRelationship.bind(this);
-    this.taskToggle = this.taskToggle.bind(this);
-  }
-
-  initState() {
-    return ({
-      userHeader: {
-        user_name: '',
-        user_image: '',
-        user_tagline: ''
-      },
-      gratitudes:[],
-      priorities: [],
-      projects: [],
-      tasks: [],
-      relationships: [],
-      contacts: []
-    })
-  }
-
-  updateUserHeader(key, value){
-    let userHeader = {...this.state.userHeader};
-    userHeader[key] = value;
-    this.setState({ userHeader });
-    return null;
-  }
-
-  resetState(event){
-    if(window.confirm('Erase all data?')) {
-      this.setState(this.initState());
-    };
-  }
-
-  deleteFromStateArray(stateKey, targetId){
-    let state = this.state[stateKey] || [];
-    let update = state.filter((e) => (e.id !== targetId));
-    this.setState({ [stateKey]: update });
-  }
-
-  addToStateArray(stateKey, value){
-    var state = this.state[stateKey] || [];
-    state.unshift(value);
-    this.setState({ [stateKey]: state });
-    return true;
-  }
-
-  deleteProject(project_id){
-    let projects = this.state.projects.filter((e) => (e.id !== project_id));
-    let tasks = this.state.tasks.filter((e) => (e.project !== project_id));
-    this.setState({ tasks, projects });
-  }
-
-  deletePriority(priority){
-    let priorities = this.state.priorities.filter((e) => (e.id !== priority));
-    let projects = this.state.tasks.filter((e) => (e.priority !== priority));
-    let tasks = this.state.tasks.filter((e) => (e.priority !== priority));
-    this.setState({ tasks, projects, priorities })
-  }
-
-  deleteRelationship(relationship){
-    this.deleteFromStateArray('relationships', relationship);
-    let contacts = this.state.contacts.filter((e) => (
-      e.relationship !== relationship
-    ));
-    this.setState({ contacts });
-  }
-
-  taskToggle(event){
-    let task_id = event.target.attributes.name.value;
-    let task = this.getSingle('tasks', task_id);
-
-    let attr = event.target.id;
-    task[attr] = event.target.checked;
-
-    let index = this.state.tasks.findIndex((e, idx) => (e.id === task_id));
-    if(index > -1) {
-      console.log('Updating!');
-      let tasks = this.state.tasks;
-      tasks[index] = task;
-      this.setState({ tasks });
-    }
-  }
-
-  getSingle(key, id){
-    return this.state[key].filter((e) => (e.id === id))[0];
-  }
-
-  getProjects(priority){
-    return this.state.projects.filter((e) => (e.priority === priority))
-  }
-
-  getTasks(project){
-    return this.state.tasks.filter((e) => (e.project === project));
-  }
-
-  getContacts(relationship){
-    if(!relationship) { return [] };
-
-    return this.state.contacts.filter((e) => (
-      e.relationship === relationship.id
-    ));
+    this.state = State.initState();
+    this.updateUserHeader = State.updateUserHeader.bind(this);
+    this.resetState = State.resetState.bind(this);
+    this.deleteFromStateArray = State.deleteFromStateArray.bind(this);
+    this.addToStateArray = State.addToStateArray.bind(this);
+    this.deleteProject = State.deleteProject.bind(this);
+    this.deletePriority = State.deletePriority.bind(this);
+    this.deleteRelationship = State.deleteRelationship.bind(this);
+    this.taskToggle = State.taskToggle.bind(this);
+    this.getSingle = State.getSingle.bind(this);
+    this.getProjects = State.getProjects.bind(this);
+    this.getTasks = State.getTasks.bind(this);
+    this.getContacts = State.getContacts.bind(this);
+    this.saveStateToStorage = State.saveStateToStorage.bind(this);
   }
 
   /* We need to save directly to localStorage for mobile apps */
   componentDidUpdate(){
     this.saveStateToStorage();
-  }
-
-  saveStateToStorage(allowNewKey = true) {
-    let prefix = "";
-    let parent = this;
-    let blacklist = [];
-
-    // loop through all of the parent's state
-    for (let key in this.state) {
-      // save item to storage if not on the blacklist
-      let prefixWithKey = `${prefix}_${key}`;
-      if (blacklist.indexOf(key) < 0 && allowNewKey) {
-        localStorage.setItem(prefixWithKey, JSON.stringify(parent.state[key]));
-      }
-    }
   }
 
   render() {
---
+++ src/lib/AppState.js
@@ -0,0 +1,115 @@
+export function initState() {
+  return ({
+    userHeader: {
+      user_name: '',
+      user_image: '',
+      user_tagline: ''
+    },
+    gratitudes:[],
+    priorities: [],
+    projects: [],
+    tasks: [],
+    relationships: [],
+    contacts: []
+  })
+}
+
+export function updateUserHeader(key, value){
+  let userHeader = {...this.state.userHeader};
+  userHeader[key] = value;
+  this.setState({ userHeader });
+  return null;
+}
+
+export function resetState(event){
+  if(window.confirm('Erase all data?')) {
+    this.setState(this.initState());
+  };
+}
+
+export function deleteFromStateArray(stateKey, targetId){
+  let state = this.state[stateKey] || [];
+  let update = state.filter((e) => (e.id !== targetId));
+  this.setState({ [stateKey]: update });
+}
+
+export function addToStateArray(stateKey, value){
+  var state = this.state[stateKey] || [];
+  state.unshift(value);
+  this.setState({ [stateKey]: state });
+  return true;
+}
+
+export function deleteProject(project_id){
+  let projects = this.state.projects.filter((e) => (e.id !== project_id));
+  let tasks = this.state.tasks.filter((e) => (e.project !== project_id));
+  this.setState({ tasks, projects });
+}
+
+export function deletePriority(priority){
+  let priorities = this.state.priorities.filter((e) => (e.id !== priority));
+  let projects = this.state.tasks.filter((e) => (e.priority !== priority));
+  let tasks = this.state.tasks.filter((e) => (e.priority !== priority));
+  this.setState({ tasks, projects, priorities })
+}
+
+export function deleteRelationship(relationship){
+  this.deleteFromStateArray('relationships', relationship);
+  let contacts = this.state.contacts.filter((e) => (
+    e.relationship !== relationship
+  ));
+  this.setState({ contacts });
+}
+
+export function taskToggle(event){
+  let task_id = event.target.attributes.name.value;
+  let task = this.getSingle('tasks', task_id);
+
+  let attr = event.target.id;
+  task[attr] = event.target.checked;
+
+  let index = this.state.tasks.findIndex((e, idx) => (e.id === task_id));
+  if(index > -1) {
+    console.log('Updating!');
+    let tasks = this.state.tasks;
+    tasks[index] = task;
+    this.setState({ tasks });
+  }
+}
+
+export function getSingle(key, id){
+  return this.state[key].filter((e) => (e.id === id))[0];
+}
+
+export function getProjects(priority){
+  return this.state.projects.filter((e) => (e.priority === priority))
+}
+
+export function getTasks(project){
+  return this.state.tasks.filter((e) => (e.project === project));
+}
+
+export function getContacts(relationship){
+  if(!relationship) { return [] };
+
+  return this.state.contacts.filter((e) => (
+    e.relationship === relationship.id
+  ));
+}
+
+
+
+export function saveStateToStorage(allowNewKey = true) {
+  let prefix = "";
+  let parent = this;
+  let blacklist = [];
+
+  // loop through all of the parent's state
+  for (let key in this.state) {
+    // save item to storage if not on the blacklist
+    let prefixWithKey = `${prefix}_${key}`;
+    if (blacklist.indexOf(key) < 0 && allowNewKey) {
+      localStorage.setItem(prefixWithKey, JSON.stringify(parent.state[key]));
+    }
+  }
+}
