18d934b40db9b3738efc531857a48629167eac29
Meissa Dia <meissadia@gmail.com>
2018-09-13 02:08:40 -0700

    Remove unused Link

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,5 +1,5 @@
 import React                from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import Toggle               from 'react-toggle';
 import PageNavigation       from './PageNavigation';
 import '../css/FormView.css';
